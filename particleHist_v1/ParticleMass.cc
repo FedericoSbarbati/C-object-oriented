f57745a486c8d5e384a2efa04ce621f2722597be
Federico Sbarbati <federicosbarbati@gmail.com>
2024-03-22 18:46:49 +0100

    Endjob updated

--- particleHist_v1/ParticleMass.cc
+++ particleHist_v1/ParticleMass.cc
@@ -3,12 +3,12 @@
 #include <sstream>
 #include <cmath>
 
-
 #include "ParticleMass.h"
 #include "Event.h"
 #include "MassMean.h"
 #include "TH1F.h"
-
+#include "TFile.h"
+#include "TDirectory.h"
 
 using namespace std;
 
@@ -27,12 +27,12 @@ void ParticleMass::pCreate(const string &name, float min, float max)
     const char *hName = name.c_str();
 
     // bin number equal to point number
-    //int nBinD = Event::minSize;
+    // int nBinD = Event::minSize;
 
     // create TH1F and statistic objects and store their pointers
-    Particle* p = new Particle;
+    Particle *p = new Particle;
     p->name = name;
-    p->mMean = new MassMean(min,max);
+    p->mMean = new MassMean(min, max);
     p->hMean = new TH1F();
     p->hMean->SetName(hName);
     p->hMean->SetTitle(hName);
@@ -46,34 +46,41 @@ void ParticleMass::beginJob()
     pList.reserve(2);
 
     // creating Particles instances
-    pCreate("K^{0}_{s}",0.495,0.500);
-    pCreate("#Lambda_{0}",1.115,1.116);
+    pCreate("K^{0}_{s}", 0.495, 0.500);
+    pCreate("#Lambda_{0}", 1.115, 1.116);
 
     return;
 }
 
 void ParticleMass::endJob()
 {
+    // save current working area
+    TDirectory *currentDir = gDirectory;
+    // open histogram file
+    TFile *file = new TFile("hist.root", "CREATE");
 
     // loop over MassMean objects
     for (Particle *p : pList)
     {
         MassMean *statMean = p->mMean;
-        TH1F *graph = p->hMean;
+        TH1F *hist = p->hMean;
 
-        //Data evalution
+        // Data evalution
         statMean->compute();
-        
+
         // Printing results:
         cout << endl
              << endl;
-        cout << "Mean: " << mMean->getMean() << endl;
-        cout << "RMS: " << mMean->getRMS() << endl;
-        cout << "Number of accepted Events: " << mMean->getNacceptedEv() << endl;
+        cout << "Mean: " << statMean->getMean() << endl;
+        cout << "RMS: " << statMean->getRMS() << endl;
+        cout << "Number of accepted Events: " << statMean->getNacceptedEv() << endl;
         cout << endl
              << endl;
+        
+        hist->Write();  
     }
 
+    file->Close();
     return;
 }
 
