ad38753fa75b5a481018d80165da3ae8bcbafb5c
Federico Sbarbati <federicosbarbati@gmail.com>
2024-03-22 15:46:23 +0100

    Root include file on Java

--- braggPlot_v1/ElementReco.cc
+++ braggPlot_v1/ElementReco.cc
@@ -13,35 +13,35 @@
 
 using namespace std;
 
-ElementReco::ElementReco() {
+ElementReco::ElementReco()
+{
 }
 
-
-ElementReco::~ElementReco() {
+ElementReco::~ElementReco()
+{
 }
-
 
 // function to be called at execution start
-void ElementReco::beginJob() {
+void ElementReco::beginJob()
+{
 
   // create energy distributions for different total energy ranges
-  bCurve.reserve( 10 );
-  bCreate( "background" , 3000.0, 5000.0 );
-  bCreate( "amaronium"  , 6000.0, 6499.0 );
-  bCreate( "barberium"  , 6500.0, 6799.0 );
-  bCreate( "cabernetium", 6800.0, 7200.0 );
+  bCurve.reserve(10);
+  bCreate("background", 3000.0, 5000.0);
+  bCreate("amaronium", 6000.0, 6499.0);
+  bCreate("barberium", 6500.0, 6799.0);
+  bCreate("cabernetium", 6800.0, 7200.0);
   return;
-
 }
 
-
 // function to be called at execution end
-void ElementReco::endJob() {
+void ElementReco::endJob()
+{
 
   // save current working area
-  TDirectory* currentDir = gDirectory;
+  TDirectory *currentDir = gDirectory;
   // open histogram file
-  TFile* file = new TFile( "hist.root", "CREATE" );
+  TFile *file = new TFile("hist.root", "CREATE");
 
   // fill distributions with mean and rms energies
 
@@ -50,27 +50,29 @@ void ElementReco::endJob() {
   int iBinD;
 
   // loop over elements
-  for ( BraggCurve* bParticle: bCurve ) {
+  for (BraggCurve *bParticle : bCurve)
+  {
     // get Bragg curve informations:
     // mean and rms energies and energy loss graph
-    BraggMean* bMean = bParticle->bMean;
-    TH1F*      hMean = bParticle->hMean;
+    BraggMean *bMean = bParticle->bMean;
+    TH1F *hMean = bParticle->hMean;
     // compute results
     bMean->compute();
     // get mean and rms energies
-    const vector<double>& mean = bMean->eMean();
-    const vector<double>& rms  = bMean->eRMS ();
+    const vector<double> &mean = bMean->eMean();
+    const vector<double> &rms = bMean->eRMS();
     // loop over points
-    for ( iBinD = 0; iBinD < nBinD; ++iBinD ) {
+    for (iBinD = 0; iBinD < nBinD; ++iBinD)
+    {
       // print results
-//      cout << iBinD << " " << mean[iBinD] << " " << rms[iBinD] << endl;
+      //      cout << iBinD << " " << mean[iBinD] << " " << rms[iBinD] << endl;
       // set center and error values in the graph
       // by using SetBinContent and SetBinError, bin count starts from 1
-      hMean->SetBinContent( iBinD + 1, mean[iBinD] );
-      hMean->SetBinError  ( iBinD + 1, rms [iBinD] );
+      hMean->SetBinContent(iBinD + 1, mean[iBinD]);
+      hMean->SetBinError(iBinD + 1, rms[iBinD]);
     }
     // print number of events
-//    cout << bMean->nEvents() << endl;
+    //    cout << bMean->nEvents() << endl;
     // save distribution
     hMean->Write();
   }
@@ -82,37 +84,35 @@ void ElementReco::endJob() {
   currentDir->cd();
 
   return;
-
 }
 
-
 // function to be called for each event
-void ElementReco::process( const Event& ev ) {
+void ElementReco::process(const Event &ev)
+{
   // loop over energy distributions and pass event to each of them
-  for ( BraggCurve* bParticle: bCurve ) bParticle->bMean->add( ev );
+  for (BraggCurve *bParticle : bCurve)
+    bParticle->bMean->add(ev);
   return;
 }
 
-
 // create objects for a Bragg curve
-void ElementReco::bCreate( const string& name, float min, float max ) {
+void ElementReco::bCreate(const string &name, float min, float max)
+{
 
   // create energy distribution for events with total energy in given range
 
   // create name for TH1F object
-  const char* hName = name.c_str();
+  const char *hName = name.c_str();
 
   // bin number equal to point number
   int nBinD = Event::minSize;
 
   // create TH1F and statistic objects and store their pointers
-  BraggCurve* bc = new BraggCurve;
-  bc-> name = name;
-  bc->bMean = new BraggMean( min, max );
-  bc->hMean = new TH1F( hName, hName, nBinD, 0.5, nBinD + 0.5 );
-  bCurve.push_back( bc );
+  BraggCurve *bc = new BraggCurve;
+  bc->name = name;
+  bc->bMean = new BraggMean(min, max);
+  bc->hMean = new TH1F(hName, hName, nBinD, 0.5, nBinD + 0.5);
+  bCurve.push_back(bc);
 
   return;
-
 }
-
