6c749fc5266983e06ccc21919cda5c2e11d2e42e
Federico Sbarbati <federicosbarbati@gmail.com>
2024-03-22 17:52:25 +0100

    Aggiornamento

--- particleHist_v1/ParticleMass.cc
+++ particleHist_v1/ParticleMass.cc
@@ -1,11 +1,14 @@
-#include "ParticleMass.h"
-#include "Event.h"
-#include "MassMean.h"
-
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <cmath>
+
+
+#include "ParticleMass.h"
+#include "Event.h"
+#include "MassMean.h"
+#include "TH1F.h"
+
 
 using namespace std;
 
@@ -20,21 +23,20 @@ ParticleMass::~ParticleMass()
 // create objects for a Bragg curve
 void ParticleMass::pCreate(const string &name, float min, float max)
 {
-
-    // create energy distribution for events with total energy in given range
-
     // create name for TH1F object
     const char *hName = name.c_str();
 
     // bin number equal to point number
-    int nBinD = Event::minSize;
+    //int nBinD = Event::minSize;
 
     // create TH1F and statistic objects and store their pointers
-    BraggCurve *bc = new BraggCurve;
-    bc->name = name;
-    bc->bMean = new BraggMean(min, max);
-    bc->hMean = new TH1F(hName, hName, nBinD, 0.5, nBinD + 0.5);
-    bCurve.push_back(bc);
+    Particle* p = new Particle;
+    p->name = name;
+    p->mMean = new MassMean(min,max);
+    p->hMean = new TH1F();
+    p->hMean->SetName(hName);
+    p->hMean->SetTitle(hName);
+    pList.push_back(p);
 
     return;
 }
@@ -44,6 +46,9 @@ void ParticleMass::beginJob()
     pList.reserve(2);
 
     // creating Particles instances
+
+    pCreate("")
+
     pList.push_back(new MassMean(0.495, 0.500));
     pList.push_back(new MassMean(1.115, 1.116));
 
