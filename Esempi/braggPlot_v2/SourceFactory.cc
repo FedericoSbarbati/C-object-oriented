2d8aad83166485bad3e6d17fd49d0b7328c212f0
Federico Sbarbati <federicosbarbati@gmail.com>
2024-03-24 16:50:47 +0100

    Commenti all'esempio

--- Esempi/braggPlot_v2/SourceFactory.cc
+++ Esempi/braggPlot_v2/SourceFactory.cc
@@ -9,34 +9,37 @@
 
 using namespace std;
 
-SourceFactory::SourceFactory() {
+SourceFactory::SourceFactory()
+{
 }
 
-
-SourceFactory::~SourceFactory() {
+SourceFactory::~SourceFactory()
+{
 }
-
 
 // create event source
-EventSource* SourceFactory::create( const AnalysisInfo* info ) {
-  if ( info->contains( "input" ) ) {
-    return new EventReadFromFile( info->value( "input" ) );
+EventSource *SourceFactory::create(const AnalysisInfo *info)
+{
+  if (info->contains("input"))
+  {
+    return new EventReadFromFile(info->value("input"));
   }
-  if ( info->contains( "sim"   ) ) {
+  if (info->contains("sim"))
+  {
     stringstream sstr;
     // get number of events to generate
-    sstr.str( info->value( "sim" ) );
+    sstr.str(info->value("sim"));
     unsigned int nevt;
     sstr >> nevt;
     // set seed if available
     unsigned int seed = 1;
-    if ( info->contains( "seed" ) ) {
+    if (info->contains("seed"))
+    {
       sstr.clear();
-      sstr.str( info->value( "seed" ) );
+      sstr.str(info->value("seed"));
       sstr >> seed;
     }
-    return new EventSim( nevt, seed );
+    return new EventSim(nevt, seed);
   }
   return nullptr;
 }
-
