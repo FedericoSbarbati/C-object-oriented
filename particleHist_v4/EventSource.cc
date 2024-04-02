783be83d3b00313107a0c11deaeda0c9217f3f45
Federico Sbarbati <federicosbarbati@gmail.com>
2024-04-02 17:20:01 +0200

    Trying to compile, some changes to include path Problem: EventScource

--- particleHist_v4/EventSource.cc
+++ particleHist_v4/EventSource.cc
@@ -1,4 +1,5 @@
 #include "EventSource.h"
+#include "Event.h"
 #include "util/include/Dispatcher.h"
 
 EventSource::EventSource()
@@ -9,13 +10,11 @@ EventSource::~EventSource()
 {
 }
 
-void EventSource::run()
-{
-    const Event *ev;
-    while ((ev = get()) != nullptr)
-    {
-        Dispatcher<Event>::notify(*ev);
-        delete ev;
-    }
-    return;
+void EventSource::run() {
+  const Event* ev;
+  while ( ( ev = get() ) != nullptr ) {
+    Dispatcher<Event>::notify( *ev );
+    delete ev;
+  }
+  return;
 }
