73da69c888aecfd4ed70cd430d9142e3d244205f
Federico Sbarbati <federicosbarbati@gmail.com>
2024-04-03 14:59:57 +0200

    Added necessary file not to modify

---
+++ particleHist_v5/ParticleReco.cc
@@ -0,0 +1,141 @@
+#include "ParticleReco.h"
+#include "Event.h"
+#include "Utilities.h"
+#include "Constants.h"
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+// constructor
+ParticleReco::ParticleReco()
+{
+}
+
+// destructor
+ParticleReco::~ParticleReco()
+{
+}
+
+// recompute tag informations for new event
+void ParticleReco::update(const Event &ev)
+{
+
+  // set default quantities for an undefined event (not physical)
+  decayType = unknown;
+  totalEnergy = -1.0;
+  invariantMass = -1.0;
+
+  // variables to loop over particles
+  int n = ev.getnParticle();
+
+  // positive / negative track counters
+  int negN = 0;
+  int posN = 0;
+
+  // variables for momentum sums
+  double pxTot = 0;
+  double pyTot = 0;
+  double pzTot = 0;
+
+  // variables for energy sums, for K0 and Lambda0
+  double EtotKO = 0;
+  double EtotLO = 0;
+
+  for (int i = 0; i < n; i++)
+  {
+    Event::part_ptr p = ev.particle(i);
+    // Updating momentum sums
+    pxTot += p->px;
+    pyTot += p->py;
+    pzTot += p->pz;
+
+    // Updating energy sums:
+    // 1-K0 hypotheses (pion mass for both particle)
+    double Ek = Utilities::energy(p->px, p->py, p->pz, Constants::massPion);
+    EtotKO += Ek;
+    // 2-L0 hypotheses
+    double EL;
+    if (p->charge > 0)
+    {
+      EL = Utilities::energy(p->px, p->py, p->pz, Constants::massProton);
+      posN++;
+    }
+    else
+    {
+      EL = Utilities::energy(p->px, p->py, p->pz, Constants::massPion);
+      negN++;
+    }
+    EtotLO += EL;
+  }
+  if (!(negN == 1 && posN == 1))
+  {
+    return; // Non updated private member-> Error handling
+  }
+
+  // code to compute total energy and invariant mass for the two
+  // mass hypotheses for the decay products
+
+  double invMassKO = Utilities::invariantMass(pxTot, pyTot, pzTot, EtotKO);
+  double invMassLO = Utilities::invariantMass(pxTot, pyTot, pzTot, EtotLO);
+
+  // compare invariant masses with known values and set final values
+  // ( type, energy and mass )
+  double prec1 = std::abs(invMassKO - Constants::massK0);
+  double prec2 = std::abs(invMassLO - Constants::massLambda0);
+
+  if (prec1 > prec2)
+  {
+    decayType = Lambda0;
+    invariantMass = invMassLO;
+    totalEnergy = EtotLO;
+    return;
+  }
+  else
+  {
+    decayType = K0;
+    invariantMass = invMassKO;
+    totalEnergy = EtotKO;
+    return;
+  }
+
+}
+
+/*
+Order of Operations:
+
+check() è LazyObserver::check();
+Se l'oggetto non è Uptodate allora chiama LazyObserver::update(*oggetto)
+Questa è virtual e abstract -> Override in ParticleReco::Update()
+
+Richiamando ParticleReco::Update() rifà eventualmente le stime e:
+UpToDate = true;
+updating = false;
+
+se no manda un warning recursive call.
+*/
+
+// return particle type
+  ParticleReco::ParticleType ParticleReco::getParticleType() 
+  {
+    // check for new event and return result
+    check();
+    return decayType;
+
+    }
+
+// return particle energy
+double ParticleReco::getParticleEnergy() 
+{
+    // check for new event and return result
+    check();
+    return totalEnergy;
+    }
+
+// return particle mass
+double ParticleReco::getParticleMass() 
+{
+  // check for new event and return result
+  check();
+  return invariantMass;
+}
---
+++ particleHist_v5/ProperTime.cc
@@ -0,0 +1,76 @@
+#include "ProperTime.h"
+#include "Event.h"
+#include "Utilities.h"
+#include "Constants.h"
+#include "ParticleReco.h"
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+// constructor
+ProperTime::ProperTime()
+{
+}
+
+// destructor
+ProperTime::~ProperTime()
+{
+}
+
+// recompute tag informations for new event
+void ProperTime::update(const Event &ev)
+{
+  decayType = unknown;
+  totalEnergy = -1.0;
+  invariantMass = -1.0;
+  static ParticleReco *ener = ParticleReco::instance();
+
+  double x = ev.getX();
+  double y = ev.getY();
+  double z = ev.getZ();
+
+  double d = sqrt(x * x + y * y + z * z);
+  double e = ener->getParticleEnergy();
+  double m = ener->getParticleMass();
+
+  double p = sqrt(e * e - m * m);
+  double c = Constants::lightVelocity;
+
+  double t = (d * m) / (p * c);
+  time = t;
+
+  return;
+}
+
+// return particle type
+ProperTime::ParticleType ProperTime::getParticleType()
+{
+  // check for new event and return result
+  check();
+  return decayType;
+}
+
+// return particle energy
+double ProperTime::getParticleEnergy()
+{
+  // check for new event and return result
+  check();
+  return totalEnergy;
+}
+
+// return particle mass
+double ProperTime::getParticleMass()
+{
+  // check for new event and return result
+  check();
+  return invariantMass;
+}
+
+// return decay time
+double ProperTime::getDecayTime()
+{
+  // check for new event and return result
+  check();
+  return time;
+}
